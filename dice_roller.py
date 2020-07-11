a8f9511a8b48de7252ca6c59a9181542570b18d6
jinjunsun <jinjun.sun@qantasloyalty.com>
2020-07-12 01:08:43 +1000

    add code for dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,11 @@
-def main():
-  #print('You rolled a die')
+import random
 
-if __name__== "__main__":
-  main()
+
+def main():
+    print('You rolled a die')
+    face_up = random.randint(1, 6)
+    print(f'you got {face_up}')
+
+
+if __name__ == "__main__":
+    main()
