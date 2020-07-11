60ba971b554f02b91f48ea7e556bdfef78e035a6
jinjunsun <jinjun.sun@qantasloyalty.com>
2020-07-12 01:25:40 +1000

    change variable name

--- dice_roller.py
+++ dice_roller.py
@@ -3,8 +3,8 @@ import random
 
 def main():
     print('You rolled a die')
-    face_up = random.randint(1, 6)
-    print(f'you got {face_up}')
+    roll = random.randint(1, 6)
+    print(f'you got {roll}')
 
 
 if __name__ == "__main__":
